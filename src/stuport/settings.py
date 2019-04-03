ab7b9c1165b5a5e16dcf93cafb47f935cc8e8a5e
AkshayPakhle10 <akshay.pakhle888@gmail.com>
2019-04-03 17:17:14 +0530

    Deployment Mods

--- src/stuport/settings.py
+++ src/stuport/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 if DEBUG:
     MEDIA_URL = '/media/'
     STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","static-only")
